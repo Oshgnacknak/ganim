2a7703a1cd787ef7996972fbd1ddccaa38841913
Oshgnacknak <osh@oshgnacknak.de>
2022-05-07 22:20:27 +0200

    Steering behavior

--- ganim.js
+++ ganim.js
@@ -1,24 +1,41 @@
 Algebra(2, 0, 1, () => {
-
+  
   const currentTime = () =>
     performance.now() / 1000;
+    
+  let last = currentTime();
+  let dt;
 
-  
-  const point = (x=0, y=0, z=0) =>
+  const createPoint = (x=0, y=0, z=0) =>
     !(x*1e1 + y*1e2 + z*1e3 + 1e0);
+    
+  const createMotor = (from, to) => {
+    let m = 1 + (to / from);
+    return m.Normalized;
+  };
   
-  let p = point(-0.17, 0.56);
+  const lerp = (t, x) => 
+    (1 - t) + t * x;
   
-  let vel = 0;
+  let position = createPoint(-1, 1);
+  let target = createPoint(1, -1);
   
-  let T = 1 - 0.0025e01;
-
   return this.graph(() => {
-    p = T >>> p;
+    let now = currentTime();
+    dt = now - last;
 
+    let motor = createMotor(position, target);
+    motor = lerp(dt, motor);
+    position = motor >>> position;
+    
+    last = now;
+    
     return [
       0x00ff00,
-      p, "p"
+      position, "position",
+      
+      0xff0000,
+      target, "target"
     ];
   }, {
     lineWidth: 4,
