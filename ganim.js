8599cc3a68249a116ebf47c7c0cf33be08a7d496
Oshgnacknak <osh@oshgnacknak.de>
2022-05-07 20:41:25 +0200

    Render point

---
+++ ganim.js
@@ -0,0 +1,16 @@
+Algebra(2, 0, 1, () => {
+  
+  const point = (x=0, y=0, z=0) =>
+    !(x*1e1 + y*1e2 + z*1e3 + 1e0);
+  
+  
+  return this.graph(() => {
+    return [
+      0x00ff00,
+      point(-0.17, 0.56), "p"
+    ];
+  }, {
+    lineWidth: 4,
+    grid: true
+  });
+});
