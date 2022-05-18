c5c7164e99d7bb832c8e91eaee9d0f10be54718a
Oshgnacknak <osh@oshgnacknak.de>
2022-05-18 18:00:13 +0200

    Line separator

---
+++ line-seperator.js
@@ -0,0 +1,55 @@
+Algebra(2, 0, 1, () => {
+
+  const currentTime = () =>
+    performance.now() / 1000;
+    
+  let last = currentTime();
+  let dt;
+
+  const createPoint = (x=0, y=0, z=0) =>
+    !(x*1e1 + y*1e2 + z*1e3 + 1e0);
+
+  
+  const innerProduct = (a, b) =>
+    0.5 * (a*b + b*a).Dual.s;
+  
+  let point = createPoint();
+  
+  let angle = 0;
+
+  return this.graph(() => {
+    let now = currentTime();
+    dt = now - last;
+    
+    angle += 0.22 * dt;
+    
+    let R = Math.E**(createPoint() * angle * Math.PI);
+    
+    let wall = R >>> (1e2 - 0e0);
+    
+    let cmp = innerProduct(point, wall);
+    let color;
+    
+    if (cmp < 0) {
+      color = 0x00ff00;
+    } else if (cmp > 0) {
+      color = 0x0000ff;
+    } else {
+      color = 0xeeeeee;
+    }
+  
+    last = now; 
+    
+    return [
+      color,
+      point, "point",
+      
+      0xff0000,
+      wall, "wall"
+    ];
+  }, {
+    lineWidth: 4,
+    grid: true,
+    animate: true
+  });
+});
