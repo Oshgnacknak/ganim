dbcae2db9aa8d0cb1a46831bc2e4fe56775da30a
Oshgnacknak <osh@oshgnacknak.de>
2022-05-22 22:31:25 +0200

    Add utilities to algebra.js

--- public/algebra.js
+++ public/algebra.js
@@ -1,4 +1,13 @@
 const Algebra = f =>
     window.Algebra(2, 0, 1, f);
 
+const utils = Algebra(() => ({
+    innerProduct: (a, b) =>
+        0.5 * (a*b + b*a).Dual.s,
+
+    createPoint: (x=0, y=0, z=0) =>
+        !(x*1e1 + y*1e2 + z*1e3 + 1e0)
+}));
+
+export const { createPoint, innerProduct } = utils;
 export default Algebra;
