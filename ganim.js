ed01371d8649159bf9811fd02b45cb07c6cd4155
Oshgnacknak <osh@oshgnacknak.de>
2022-05-28 19:22:33 +0200

    Four walls and many particles

--- ganim.js
+++ ganim.js
@@ -3,6 +3,14 @@ Algebra(2, 0, 1, () => {
   const currentTime = () =>
     performance.now() / 1000;
     
+  const createColor = (r, g, b) =>
+    Math.round(r) * 2**16
+    | Math.round(g) * 2**8
+    | Math.round(b)
+    
+  const random = (min, max) =>
+    Math.random() * (max - min) + min;
+    
   let last = currentTime();
   let dt;
   
@@ -25,17 +33,22 @@ Algebra(2, 0, 1, () => {
     
   class Particle {
     constructor() {
+      this.color = createColor(random(50, 255), random(50, 255), random(50, 255));
       this.motor = 1;
-      this.vel = (0.2e1 - 0e0) ^ (0.3e2 - 3e0);
+      this.vel = (1e1 - random(0.33, 33)*1e0) ^ (0.3e2 - random(0.33, 0.33)*1e0);
     }
     
     positon() {
       return this.motor >>> createPoint();
     }
     
+    render() {
+      return [this.color, this.positon()];
+    }
+    
     forques() {
-      const gravity = !(~this.motor >>> -2e02);
-      const damping = !(-0.55 * this.vel);
+      const gravity = !(~this.motor >>> -5e02);
+      const damping = !(-0.12 * this.vel);
       return gravity + damping;
     }
     
@@ -55,8 +68,8 @@ Algebra(2, 0, 1, () => {
   }
   
   class Wall {
-    constructor() {
-      this.theWall = -0.2e1 + 1e2 + 1e0;
+    constructor(theWall) {
+      this.theWall = theWall;
     }
     
     render() {
@@ -67,36 +80,62 @@ Algebra(2, 0, 1, () => {
       let pos = particle.positon();
       let cmp = innerProduct(pos, this.theWall).Dual.s;
       
-      if (cmp <= 0) {
+      if (cmp < 0) {
         particle.vel = this.theWall >>> particle.vel;
+        particle.vel = particle.vel * 0.995;
         
         let intersect = project(pos, this.theWall);
         let correction = createMotor(pos, intersect);
-        correction = lerp(1, correction, 2);
+        correction = lerp(1, correction, 3);
 
         particle.motor = particle.motor * correction;
       }
     }
+    
+    rotate(dt) {
+      let R = Math.E**(createPoint(0.4, -0.2) * 0.2 * dt);
+      this.theWall = R >>> this.theWall;
+    }
   }
   
-  let particle = new Particle();
-  let wall = new Wall();
+  let particles = [];
+  for (let i = 0; i < 50; i++) {
+    particles.push(new Particle());
+  }
+  const d = 1;
+  let walls = [
+    new Wall(1e2 + d*1e0),
+    new Wall(-1e2 + d*1e0),
+    new Wall(1e1 + d*1e0),
+    new Wall(-1e1 + d*1e0)
+  ];
 
   return this.graph(() => {
     let now = currentTime();
     dt = now - last;
+    
+    for (const p of particles) {
+      p.update(dt);   
+  
+      for (const w of walls) {
+        w.bounce(p);
+      }
+    }
+  
+    for (const w of walls) {
+      w.rotate(dt);
+    }
 
-    wall.bounce(particle);
-    particle.update(dt);
 
     last = now;
     
     return [
-      0x00ff00,
-      particle.positon(),
+      ...particles.map(p =>
+        p.render()).flat(),
       
       0xff0000,
-      wall.render()
+      ...walls.map(w =>
+        w.render())
     ];
   }, {
     lineWidth: 4,
